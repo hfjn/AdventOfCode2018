425400e00fda52c3f14cbb6e9a6432d77708d823
Jannik Hoffjann <hi@hfjn.de>
2018-12-01 13:19:40 +0100

    Added go project layout

--- cmd/root.go
+++ cmd/root.go
@@ -1,42 +1,99 @@
 package cmd
 
 import (
-        "fmt"
-        "os"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 
-        "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-        Use:   "advent",
-        Short: "A brief description of your application",
-        Long: `A longer description that spans multiple lines and likely contains
+	Use:   "advent",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-        // Uncomment the following line if your bare application
-        // has an action associated with it:
-        //      Run: func(cmd *cobra.Command, args []string) { },
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//      Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-        if err := rootCmd.Execute(); err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	calculate()
 }
 
 func init() {
-        cobra.OnInitialize()
+	cobra.OnInitialize()
 
-        // Cobra also supports local flags, which will only run
-        // when this action is called directly.
-        // rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+const check_occurences = true
+const filename = "input.txt"
+
+func calculate() {
+
+	numbers := read_file(filename)
+
+	current := 0
+	occurrences := make(map[int]int)
+
+	found := false
+
+	for !found {
+		for _, number := range numbers {
+			current = current + number
+			if check_occurences {
+				occurrences[current]++
+				if occurrences[current] > 1 {
+					fmt.Printf("%s%d", "first duplicate occurence is: ", current)
+					os.Exit(0)
+				}
+			}
+		}
+	}
+
+	fmt.Println(current)
+
+}
+
+func read_file(filename string) []int {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var numbers []int
+
+	for scanner.Scan() {
+		number, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(2)
+		}
+		numbers = append(numbers, number)
+	}
+
+	return numbers
 }
--- cmd/version.go
+++ cmd/version.go
@@ -1,25 +1,26 @@
 package cmd
 
 import (
-        "github.com/spf13/cobra"
-        "fmt"
-        "github.com/hfjn/advent/version"
+	"fmt"
+
+	"github.com/hfjn/advent/version"
+	"github.com/spf13/cobra"
 )
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
-        Use:   "version",
-        Short: "Print the version number of advent",
-        Long:  `All software has versions. This is advent`,
-        Run: func(cmd *cobra.Command, args []string) {
-                fmt.Println("Build Date:", version.BuildDate)
-                fmt.Println("Git Commit:", version.GitCommit)
-                fmt.Println("Version:", version.Version)
-                fmt.Println("Go Version:", version.GoVersion)
-                fmt.Println("OS / Arch:", version.OsArch)
-        },
+	Use:   "version",
+	Short: "Print the version number of advent",
+	Long:  `All software has versions. This is advent`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Build Date:", version.BuildDate)
+		fmt.Println("Git Commit:", version.GitCommit)
+		fmt.Println("Version:", version.Version)
+		fmt.Println("Go Version:", version.GoVersion)
+		fmt.Println("OS / Arch:", version.OsArch)
+	},
 }
 
 func init() {
-        rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(versionCmd)
 }
